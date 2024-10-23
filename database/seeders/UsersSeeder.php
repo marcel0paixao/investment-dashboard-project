d52d1c123fced687beaa321fcc36459b0532f9f9
marcel0paixao <marceloht461@gmail.com>
2024-10-23 00:15:42 -0300

    add sanctum, api, kernel

---
+++ database/seeders/UsersSeeder.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class UsersSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        User::factory()->create([
+            'name' => 'Administrador',
+            'email' => 'admin@email.com',
+            'password' => Hash::make('Admin123')
+        ]);
+    }
+}
